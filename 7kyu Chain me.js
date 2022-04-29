c7faa151937e2b037a5ba9ccf8579b697868b2fa
pan-alex <alexanderypan@gmail.com>
2022-04-28 21:25:58 -0400

    Create 7kyu Chain me.js

---
+++ 7kyu Chain me.js
@@ -0,0 +1,28 @@
+// Write a generic function chainer
+
+// Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for Ruby).
+
+// The input for each function is the output of the previous function (except the first function, which takes the starting value as its input). Return the final value after execution is complete.
+
+// function add(num) {
+//     return num + 1;
+//   }
+
+//   function mult(num) {
+//     return num * 30;
+//   }
+
+//   chain(2, [add, mult]);
+  // returns 90;
+
+function chain(input, fs) {
+    for (i=0; i<fs.length; i++) {
+        input = fs[i](input)
+    }
+    return input
+}
+
+
+function chain(input, fs) {
+    return fs.reduce( (x, f) => f(x), input)
+}
