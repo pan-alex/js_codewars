29f8a4c4cabf9815d8e60fab3ce3ff8808c66883
pan-alex <alexanderypan@gmail.com>
2022-06-03 23:03:21 -0400

    Create 7kyu 5 without alphanums.js

---
+++ Miscellaneous/7kyu 5 without alphanums.js
@@ -0,0 +1,11 @@
+// Write a function/proc/lambda called _ that always returns 5.
+
+// Sounds easy right? Just bear in mind that you can only use the following characters:
+
+// ~!@#$%^&*()-_+=[]\{}|:;"'<,>.?/`
+
+// Kata inspired by 5 without numbers !!.
+
+// Good luck :)
+
+_=()=>!!_+!!_+!!_+!!_+!!_
