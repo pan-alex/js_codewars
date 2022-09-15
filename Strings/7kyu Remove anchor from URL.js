d8ddb0e2adb1678ea361e1c8d99455b11260d6d1
pan-alex <alexanderypan@gmail.com>
2022-09-14 20:39:46 -0400

    Create 7kyu Remove anchor from URL.js

---
+++ Strings/7kyu Remove anchor from URL.js
@@ -0,0 +1,14 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+// Examples
+
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+// Regular Expressions
+// Strings
+// Fundamentals
+
+
+function removeUrlAnchor(url){
+    return url.split('#')[0]
+}
